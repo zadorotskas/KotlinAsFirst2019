7c33ac970ff50759b7e5e6fa5834f24e4e958cf5
zadorotskas <zadorotskas@yandex.ru>
2019-09-05 15:46:40 +0300

    I've done

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -96,8 +96,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int =
-    (number / 100) % 10
+fun thirdDigit(number: Int): Int = (number / 100) % 10
 
 /**
  * Простая
@@ -116,8 +115,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double =
-    initial * (1 + percent / 100.0) * (1 + percent / 100.0) * (1 + percent / 100.0) * 1.0
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val k = 1 + percent / 100.0
+    return initial * k * k * k
+}
 
 /**
  * Простая
